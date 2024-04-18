3359f8f0f20b083f3a41d63ba8d512b25ce7e89b
777ParallelClematis <erin-colane@live.nmit.ac.nz>
2024-04-18 13:46:45 +1200

    working with readline for now

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,11 +1,16 @@
-const readlineSync = require('readline-sync'); // Readline is an API that is part of the node.js package
+const readline = require('readline'); // Readline is an API that is part of the node.js package
 
 const rl = readline.Interface({ // creates an "instance" of readline - enables it to be used in the rest of the program
     input: process.stdin, // enables input to happen - 
     output: process.stdout // enables output to happen
 })
- let board = [
+ let board = [ //creating the board as an array, this will later allow me to access each 'cell' by the index functions eg// board [1][2]
     [["-"], ["-"], ["-"]],
     [["-"], ["-"], ["-"]],
     [["-"], ["-"], ["-"]], 
  ];
+// function to reveal the board. 
+ function boardReveal() {
+    console.log(board.map)
+ }
+
