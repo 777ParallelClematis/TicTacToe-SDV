461f3839059603d021ea0eafa9e93ccd208cd5c2
777ParallelClematis <erin-colane@live.nmit.ac.nz>
2024-04-18 13:26:56 +1200

    readline vs readline sync battle

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,13 +1,6 @@
-const readlineSync = require('readline-sync');
+const readlineSync = require('readline-sync'); // Readline is an API that is part of the node.js package
 
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const rl = readline.Interface({ // creates an "instance" of readline - enables it to be used in the rest of the program
+    input: process.stdin, // enables input to happen - 
+    output: process.stdout // enables output to happen
 })
-grid = 
-[["[-]", "[-]", "[-]"],
-["[-]", "[-]", "[-]"], 
-["[-]", "[-]", "[-]"]].toString
-
-console.log(grid)
