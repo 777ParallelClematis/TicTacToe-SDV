3452dbd2c7892358f50ad8da98dfbff5eff4da8c
Erin Colane MacBook <erinc@Erins-Air.ip>
2024-04-24 18:50:20 +1200

    many changes made. need to sort else/if statements, win conditions etc.

--- TicTacToe.js
+++ TicTacToe.js
@@ -3,23 +3,32 @@ const readlineSync = require('readline-sync'); // Readline is an API that is par
 
 
 let board = [ //creating the board as an array, this will later allow me to access each 'cell' by the index functions eg// board [1][2]
-["-", "-", "-"],
-["-", "-", "-"],
-["-", "-", "-"]] 
+[" - ", " - ", " - "],
+[" - ", " - ", " - "],
+[" - ", " - ", " - "]] 
+let row = 1
+let col = 1
+// determining what turn is assigned either X or O. Player X will go first for the first turn. The cycle increments with every valid input. 
+let turn = 1;
+let currentUser;
+if (turn % 2 === 0) {
+  currentUser = "X";
+} else {
+  currentUser = "O";
+}
 
- let turn = 0
 // FUNCTIONS 
 
 // function to reveal the board
  function boardReveal() {
-    console.log(board.map(row => row.join (" | ")).join("\n---------\n"))// first this joins the rows, and then the columns. escape c
+    console.log(board.map(row => row.join ("|")).join("\n------------\n"))// first this joins the rows, and then the columns. escape characters "\n" create a new line in the terminal
  }
-
+// function to check if the board is full 
  function isBoardFull () {
     return board.every(row => row.every(cell => cell !==" - "))
  }
-// function to check if there is a winner. Vertically, horizontally and diagonals
- function winCheck() {
+// function to check if there is a winner. Vertically, horizontally and diagonals. Need to adjust these to work with the adjustments I made. 
+ /*function winCheck() {
     // Check verticals
 for (let i = 0; i < 3; i++) {
 if ((board[0][i] === board[1][i] && board[1][i] === board[2][i]) && (board[0][i] === "X" || board[0][i] === "O")) {
@@ -37,29 +46,43 @@ if ((board[0][0] === board[1][1] && board[1][1] === board[2][2] || board[0][2] =
     (board[1][1] === "X" || board[1][1] === "O")) {return true}
 return false;
 }
+*/
 // function to check the format of user input, after knowing its not "exit" to stop the game. It checks that the input is two numbers (1, 2 or 3) separated by a space. 
-function checkFormat(){
-var regex = /^(1|2|3)\s(1|2|3)$/
+function checkFormat(input) {
+    var regex = /^(1|2|3)\s(1|2|3)$/
+    if (regex.test(input)) {
+        return true
+    } else {
+        return false
+    }
 }
-
-if (regex.test(input)) {
-    return true} else{
-        return false }
-
 
 // function to get user coordinate placement, validate 
 function getUserCoordinatePlacement() {
-const userInput = readlineSync.question("Please enter the numeric coordinates of where you;d like your tile to be placed, eg// 1 2 OR (1,2)")
- if (userInput.stringToLowerCase = "exit") {
+let userInput = readlineSync.question(`Player ${currentUser} Please enter the numeric coordinates of where you/'d like your tile to be placed, separated by a space, eg// 1 2:  `)
+ if (userInput.toLowerCase() == "exit") {
     console.log("You exited the game. Try again any time! Remember, enter 'node TicTacToe.js' into your terminal to play again ")
-    process.exit(0)
+    process.exit(1)
  }
  
- else checkFormat(input)
- 
- {(input) => {const [row, col] = input.split(" ").map(num => parseInt(num))}
+else if (checkFormat(userInput) == true){
+    console.log(userInput)
+row = userInput.substring(2,3)// first character represents position
+col = userInput.substring(0,1) // second represents number of characrers retrieved
+console.log(`row: ${row} col: ${col}`)
+   row--
+   col--
+    if (cellIsEmpty = true) {
+     board[col][row] = currentUser
+     turn++
+     boardReveal()
+     console.log(col + " " + row)
+    } else {
+        console.log("This cell is already taken! Try again")
+        getUserCoordinatePlacement()
+    }
 }
-
+getUserCoordinatePlacement()
 }
 function embark() {
     console.log(" Welcome to  \n T | I | C \n --------- \n T | A | C \n --------- \n T | O | E \n This is a hotseat game, ask a friend to join you or verse yourself \n Have fun!")
